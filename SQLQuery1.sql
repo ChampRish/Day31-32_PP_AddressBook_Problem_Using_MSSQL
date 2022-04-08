ce43712df350f06bea61bd7f1b00047334aec73b
RISHABH <gguptarishabh82@gmail.com>
2022-04-08 22:33:13 +0530

    Getting number of contact person count by type

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -59,3 +59,6 @@ UPDATE ContactsTable set PersonType='Friends' where AdrBookName='AB1'
 UPDATE ContactsTable set PersonType='Family' where AdrBookName='AB2'
 UPDATE ContactsTable set PersonType='Profession' where AdrBookName='AB3'
 SELECT * FROM ContactsTable
+
+/*UC10 Ability to get number of contact persons i.e. count by type */
+SELECT PersonType,COUNT(PersonType) as count from ContactsTable group by PersonType
