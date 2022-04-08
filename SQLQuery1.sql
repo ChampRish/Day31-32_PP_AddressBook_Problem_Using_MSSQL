f7647c7e813a62a7762b8c61ed7f5c256c9ac7df
RISHABH <gguptarishabh82@gmail.com>
2022-04-08 21:45:00 +0530

    Contact table created in Address book database.

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,3 +2,16 @@
 create database AddressBookServiceDB
 
 use AddressBookServiceDB
+
+/* UC2 create a AddressBook table in the Address Book ServiceDB*/
+create table ContactsTable(
+Id int identity(1,1) NOT NULL PRIMARY KEY,
+FirstName varchar(16) ,
+LastName varchar(16),
+Address varchar(24),
+City varchar(16),
+State varchar(16),
+ZipCode varchar(6),
+PhoneNumber varchar(16),
+EmailId varchar(24)
+);
